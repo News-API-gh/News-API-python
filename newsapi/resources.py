a2f56b122f3371d2e1a9f6595a80cc3ec555610a
James Christopher <jcahall@washington.edu>
2018-04-25 21:05:04 -0700

    refactor

--- newsapi/resources.py
+++ newsapi/resources.py
@@ -4,33 +4,28 @@ from .client import Client
 class BaseResource:
 
     endpoint = None
+    item = None
 
     def __init__(self):
         self.client = Client()
 
     def list(self, **params):
-        return self.client.get(self.endpoint, params)
+        return self.client.get(self.endpoint, params)[self.item]
 
 
 class TopHeadlines(BaseResource):
 
     endpoint = 'top-headlines'
-
-    def list(self, **params):
-        return super().list(**params)['articles']
+    item = 'articles'
 
 
 class Everything(BaseResource):
 
     endpoint = 'everything'
-
-    def list(self, **params):
-        return super().list(**params)['articles']
+    item = 'articles'
 
 
 class Sources(BaseResource):
 
     endpoint = 'sources'
-
-    def list(self, **params):
-        return super().list(**params)['sources']
+    item = 'sources'
